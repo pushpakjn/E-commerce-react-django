b1b3b5393345f8e81f49c5d4cf2173f7f4978eb3
Pushpak Jain <officialpjain@gmail.com>
2024-02-09 03:28:10 +0530

    host

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 from datetime import timedelta
-
+import dj_database_url
+import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 SECRET_KEY = "django-insecure-!#dkqgm_u@_ow7$8g58@p1kh30oj&vwa%2h6sb1ew)ta2nvx#m"
 
 DEBUG = True
@@ -107,14 +109,17 @@ AUTH_USER_MODEL = "accounts.UserModel"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+GG = os.environ.get("DATABASE_URL")
+print(GG)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(env('DATABASE_URL'))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -14,6 +15,8 @@ urlpatterns = [
     # re_path('.*',  TemplateView.as_view(template_name='index.html')),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
