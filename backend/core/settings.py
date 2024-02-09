b03d40baadefdb01843a74f19891de594a4fbf69
Pushpak Jain <officialpjain@gmail.com>
2024-02-09 18:50:35 +0530

    corsall

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -72,6 +72,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "https://e-commerce-react-django.vercel.app", 
 #     "http://localhost:3000",  # React
