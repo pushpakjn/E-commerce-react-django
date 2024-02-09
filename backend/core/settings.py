c92f6da84f0cfc2bcb6df09585687efacdd544e8
Pushpak Jain <officialpjain@gmail.com>
2024-02-09 21:43:45 +0530

    cloud4

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -183,9 +183,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Cloudinary
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": env("Blog"),
-    "API_KEY": env("253397266762396"),
-    "API_SECRET": env("Dprx7cx446mEToPK5-AaWj_hdr8"),
+    "CLOUD_NAME": "Blog",
+    "API_KEY": "253397266762396",
+    "API_SECRET": "Dprx7cx446mEToPK5-AaWj_hdr8",
     # "CLOUD_NAME": env("CLOUD_NAME"),
     # "API_KEY": env("API_KEY"),
     # "API_SECRET": env("API_SECRET"),
