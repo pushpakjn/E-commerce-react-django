b6b6daa62ef0a8184b20030c91d03057f99ef761
Pushpak Jain <officialpjain@gmail.com>
2024-02-09 05:39:33 +0530

    proxy3

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -72,7 +72,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ALLOWED_ORIGINS = ["https://e-commerce-react-django.vercel.app"]
+CORS_ALLOWED_ORIGINS = ['*']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
