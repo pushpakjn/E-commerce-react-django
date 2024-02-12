eb910b467a68e1ead87fbb4439945929d60932ae
Pushpak Jain <officialpjain@gmail.com>
2024-02-12 19:05:21 +0530

    change

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -10,9 +10,9 @@ function ProductList({product}) {
   return (
     <Card
       imgAlt={product.name}
-      imgSrc={product.image}
+      imgSrc={`https://res.cloudinary.com/dplbssi25/${product.image}`}
     >
-      {/* console.log(product.image) */}
+      console.log(product.image)
 
       <Link to={`/product/${product.slug}`}>
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
